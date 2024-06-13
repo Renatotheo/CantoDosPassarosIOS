ea36c572d0442058b2f3cf53372470d5ebb2f25f
renato <renatotheobaldo@gmail.com>
2024-06-13 15:22:22 -0300

    Initial Commit

---
+++ CantoDosPassarosIOS/CantoDosPassarosIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CantoDosPassarosIOSApp.swift
+//  CantoDosPassarosIOS
+//
+//  Created by Renato on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CantoDosPassarosIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
